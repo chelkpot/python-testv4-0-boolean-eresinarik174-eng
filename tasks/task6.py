b2423cb2916652b2e32143770ca4ce3cdee81ba1
student <student@corp.kpiot.ru>
2025-10-16 13:32:05 +0500

    jhjh

--- tasks/task6.py
+++ tasks/task6.py
@@ -2,7 +2,9 @@
 
 def solve():
 # Ниже пишите решение задачи
-
+a, b, c = sorted(map(int, input().split()))
+result = c**2 == a**2 + b**2
+print(result)
    
    
 # Код ниже не трогать! он нужен для тестов
